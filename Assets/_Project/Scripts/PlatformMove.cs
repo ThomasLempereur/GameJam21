c83db43410a72400562d069f23598cbb001fc38d
ThomasLempereur <tho.lempereur@gmail.com>
2021-11-04 15:24:06 +0100

    first map and lobby

---
+++ Assets/_Project/Scripts/PlatformMove.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMove : MonoBehaviour
+{
+
+    [SerializeField] private Transform[] waypoints;
+    [SerializeField] private float speed;
+
+    private Transform target;
+    private int destPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = waypoints[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+        if (Vector3.Distance(transform.position, target.position) < 0.3f)
+        {
+            destPoint = (destPoint + 1) % waypoints.Length;
+            target = waypoints[destPoint];
+        }
+    }
+}
